7599c5daac88bd061466c0f25db790ab6781c27d
echomonk <erdaloylupinar@gmail.com>
2023-03-05 10:01:01 +0300

    first commit

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,59 @@
+import { Box, Button, Typography } from "@mui/material";
+import React from "react";
+import mealsImage from "../../assets/meals.jpg";
+
+const Header = () => {
+  return (
+    <React.Fragment>
+      <Box
+        sx={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "5rem",
+          backgroundColor: "#8a2b06",
+          color: "white",
+          display: "flex",
+          alignItems: "stretch",
+          justifyContent: "space-between",
+          padding: "0 10%",
+          boxShadow: "0 2px 8px rgba(0, 0, 0, 0.25)",
+          zIndex: 10,
+        }}
+      >
+        <Typography variant="h4">ReactMeals</Typography>
+        <Button
+          sx={{
+            color: "white",
+            backgroundColor: "#5a1613",
+          }}
+        >
+          Cart
+        </Button>
+      </Box>
+      <Box
+        sx={{
+          position: "relative",
+          width: "100%",
+          height: "25rem",
+          zIndex: 0,
+          overflow: "hidden",
+        }}
+      >
+        <img
+          width="110%"
+          height="100%"
+          style={{
+            objectFit: "cover",
+            transform: "rotateZ(-5deg) translateY(-4rem) translateX(-1rem)",
+          }}
+          src={mealsImage}
+          alt={"A Table Full of Delicious Food"}
+        />
+      </Box>
+    </React.Fragment>
+  );
+};
+
+export default Header;
